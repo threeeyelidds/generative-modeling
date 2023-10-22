521f96730b32510639d76b041f2e5eacbff1c44b
threeeyelidds <youqunew@gmail.com>
2023-10-21 21:16:49 -0400

    ddpm

--- diffusion/model.py
+++ diffusion/model.py
@@ -33,30 +33,30 @@ class DiffusionModel(nn.Module):
         # TODO 3.1: Compute the cumulative products for current and
         # previous timesteps.
         ##################################################################
-        self.alphas_cumprod = None
-        self.alphas_cumprod_prev =  None
-
+        alphas_with_one = torch.cat([torch.ones_like(alphas[:1]).cuda(), alphas])
+        self.alphas_cumprod = torch.cumprod(alphas_with_one, dim=0)
+        self.alphas_cumprod_prev =  torch.cat([torch.ones_like(alphas[:1]).cuda(), self.alphas_cumprod[:-1]])
         ##################################################################
         # TODO 3.1: Pre-compute values needed for forward process.
         ##################################################################
         # This is the coefficient of x_t when predicting x_0
-        self.x_0_pred_coef_1 = None
+        self.x_0_pred_coef_1 = 1/torch.sqrt(self.alphas_cumprod)
         # This is the coefficient of pred_noise when predicting x_0
-        self.x_0_pred_coef_2 = None
+        self.x_0_pred_coef_2 = -torch.sqrt(1-self.alphas_cumprod)/torch.sqrt(self.alphas_cumprod)
 
         ##################################################################
         # TODO 3.1: Compute the coefficients for the mean.
         ##################################################################
         # This is coefficient of x_0 in the DDPM section
-        self.posterior_mean_coef1 = None
+        self.posterior_mean_coef1 = torch.sqrt(alphas)*(1-self.alphas_cumprod_prev)/(1-self.alphas_cumprod)
         # This is coefficient of x_t in the DDPM section
-        self.posterior_mean_coef2 = None
+        self.posterior_mean_coef2 = torch.sqrt(self.alphas_cumprod_prev)*self.betas/(1-self.alphas_cumprod)
 
         ##################################################################
         # TODO 3.1: Compute posterior variance.
         ##################################################################
         # Calculations for posterior q(x_{t-1} | x_t, x_0) in DDPM
-        self.posterior_variance = None
+        self.posterior_variance = self.betas*(1-self.alphas_cumprod_prev)/(1-self.alphas_cumprod)
         ##################################################################
         #                          END OF YOUR CODE                      #
         ##################################################################
@@ -88,8 +88,11 @@ class DiffusionModel(nn.Module):
         # Hint: You can use extract function from utils.py. See
         # get_posterior_parameters() for usage examples.
         ##################################################################
-        pred_noise = None
-        x_0 = None
+        pred_noise = self.model(x_t,t)
+        x_0 = (
+            extract(self.x_0_pred_coef_1, t, x_t.shape) * x_t +
+            extract(self.x_0_pred_coef_2, t, x_t.shape) * pred_noise
+        )
         ##################################################################
         #                          END OF YOUR CODE                      #
         ##################################################################
@@ -104,8 +107,11 @@ class DiffusionModel(nn.Module):
         # Hint: To do this, you will need a predicted x_0. You should've
         # already implemented a function to give you x_0 above!
         ##################################################################
-        pred_img = None
-        x_0 = None
+        x_0 = self.model_predictions(x, t)
+        posterior_mean, posterior_variance, posterior_log_variance_clipped = self.get_posterior_parameters(x_0, x, t)
+        z = torch.randn_like(x)
+        pred_img = posterior_mean + torch.sqrt(posterior_variance) * z
+        
         ##################################################################
         #                          END OF YOUR CODE                      #
         ##################################################################
